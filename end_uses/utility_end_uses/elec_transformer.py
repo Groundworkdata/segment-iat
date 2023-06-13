82213414cebdf7c08c61401e0e04db08b155371b
Conor Lyman <clyman23@gmail.com>
2023-06-13 10:32:22 -0400

    Updates xmfr to use estimated power factor and allow multiple upgrades
    Updates xmfr to use estimated power factor and allow multiple upgrades
    
    Updates comments

--- end_uses/utility_end_uses/elec_transformer.py
+++ end_uses/utility_end_uses/elec_transformer.py
@@ -4,9 +4,14 @@ Defines Gas Service end use
 from typing import Dict
 import pandas as pd
 import numpy as np
+import warnings
 
 from end_uses.utility_end_uses.utility_end_use import UtilityEndUse
 from collections import Counter
+
+
+POWER_FACTOR = 0.8
+UNIT_UPGRADE_COST = 2000
 
 
 class ElecTransformer(UtilityEndUse):
@@ -35,8 +40,9 @@ class ElecTransformer(UtilityEndUse):
         self.annual_total_energy_use: dict = []
         self.annual_peak_energy_use: list = []
         self.annual_energy_use_timeseries: dict = []
+        self.annual_upgrades: list = []
 
-        self.required_upgrade_year: int = 0
+        self.required_upgrade_year: list = []
         self.upgrade_cost: list = []
         self.overloading_flag: list = []
         self.overloading_ratio: list = []
@@ -91,56 +97,69 @@ class ElecTransformer(UtilityEndUse):
             annual_peak.append(self.annual_energy_use_timeseries[i].max())
 
         return annual_peak
-    
-    def get_upgrade_year(self) -> int:
-        """
-        If we exceed the transformer capacity, return the year where this happens. Also, update the
-        total bank_KVA to account for this upgrade
 
-        NOTE: This is limited to upgrading the transformer ONCE and it doubles the transformer
-        capacity. Therefore, if the load reaches over 2x the transformer capacity, this function
-        will not account for that.
+    def get_upgrade_year(self) -> list:
         """
+        If we exceed the transformer capacity, return the years where this happens. Also, update the
+        total bank_KVA to account for this upgrade. We also calculate a list of how many upgrades we
+        make each year.
+        """
+        upgrade_years = []
+        transformer_upgrades = 0
+        annual_transformer_upgrades = np.zeros(len(self.years_vector))
+
         for years, load in zip(enumerate(self.years_vector), self.annual_peak_energy_use):
             year_idx = years[0]
             year = years[1]
 
-            if load > self.annual_bank_KVA[year_idx] * 1000:
+            while load > self.annual_bank_KVA[year_idx] * POWER_FACTOR:
                 #TODO: Refactor. Kind of ugly
                 bank_kva = np.array(self.annual_bank_KVA)
-                bank_kva[year_idx:] *= 2
+                bank_kva[year_idx:] += self._bank_kva
                 self.annual_bank_KVA = bank_kva.tolist()
-                return year
-        return 0
+
+                transformer_upgrades += 1
+                annual_transformer_upgrades[year_idx] += 1
+                upgrade_years.append(year)
+
+                if transformer_upgrades > 10:
+                    warnings.warn("Maxed out after 10 transformer upgrades!")
+                    break
+
+        self.annual_upgrades = annual_transformer_upgrades.tolist()
+
+        return upgrade_years
     
     def update_is_replacement_vector(self) -> list:
         retrofit_vector = np.zeros(len(self.years_vector))
         if self.required_upgrade_year:
-            retrofit_vector[self.required_upgrade_year - self.sim_start_year:] = 1
+            retrofit_vector[self.required_upgrade_year[0] - self.sim_start_year:] = 1
 
         return retrofit_vector.astype(bool).tolist()
     
     def update_retrofit_vector(self) -> list:
         retrofit_vector = np.zeros(len(self.years_vector))
         if self.required_upgrade_year:
-            retrofit_vector[self.required_upgrade_year - self.sim_start_year] = 1
+            retrofit_vector[[
+                upgrade_year - self.sim_start_year
+                for upgrade_year in self.required_upgrade_year
+            ]] = 1
 
         return retrofit_vector.astype(bool).tolist()
 
     def get_upgrade_cost(self) -> list:
         upgrade_cost = np.zeros(len(self.years_vector))
 
-        if self.required_upgrade_year:
-            #FIXME: Placeholder upgrade cost
-            upgrade_cost[self.required_upgrade_year - self.sim_start_year] = 2000
+        for year_idx, upgrades in enumerate(self.annual_upgrades):
+            upgrade_cost[year_idx] = UNIT_UPGRADE_COST * upgrades
 
         return upgrade_cost.tolist()
 
     def get_overloading_status(self) -> None:
         self.overloading_flag = (
-            np.array(self.annual_peak_energy_use) > (np.array(self.annual_bank_KVA) * 1000)
+            np.array(self.annual_peak_energy_use) > (np.array(self.annual_bank_KVA) * POWER_FACTOR)
         ).astype(int).tolist()
 
         self.overloading_ratio = (
-            np.array(self.annual_peak_energy_use) / (np.array(self.annual_bank_KVA) * 1000)
+            np.array(self.annual_peak_energy_use) / (np.array(self.annual_bank_KVA) * POWER_FACTOR)
         ).tolist()
